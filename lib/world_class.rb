608993b626d1d24d69436cd3504ea2479ce66af8
Tara Craig <tara.craig7@gmail.com>
2018-07-18 13:39:13 -0600

    Add method and test for active players by position

--- lib/world_class.rb
+++ lib/world_class.rb
@@ -8,4 +8,12 @@ class WorldCup
     @teams = teams
   end
 
+  def active_players_by_position(position)
+    @teams.map do |team|
+      team.players.find_all do |player|
+        player.position == position
+      end
+    end.flatten
+  end
+
 end
--- test/world_class_test.rb
+++ test/world_class_test.rb
@@ -14,5 +14,39 @@ class WorldCupTest < Minitest::Test
     assert_instance_of WorldCup, world_cup
   end
 
+  def test_it_has_attributes
+    france = Team.new("France")
+    croatia = Team.new("Croatia")
+    world_cup = WorldCup.new(2018, [france, croatia])
+
+    assert_equal 2018, world_cup.year
+    assert_equal [france, croatia], world_cup.teams
+  end
+
+  def test_it_can_find_active_players_by_positions
+    france = Team.new("France")
+    mbappe = Player.new("Kylian Mbappe", :forward)
+    griezmann = Player.new("Antoine Griezmann", :forward)
+    pogba = Player.new("Paul Pogba", :midfielder)
+
+    croatia = Team.new("Croatia")
+    modric = Player.new("Luka Modric", :midfielder)
+    perisic = Player.new("Ivan Perisic", :forward)
+    vida = Player.new("Domagoj Vida", :defender)
+
+    world_cup = WorldCup.new(2018, [france, croatia])
+
+    france.add_player(mbappe)
+    france.add_player(griezmann)
+    france.add_player(pogba)
+
+    croatia.add_player(modric)
+    croatia.add_player(perisic)
+    croatia.add_player(vida)
+
+    expected = [mbappe, griezmann, perisic]
+
+    assert_equal expected, world_cup.active_players_by_position(:forward)
+  end
 
 end
