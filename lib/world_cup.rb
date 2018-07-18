c481b225f61bfaf8eddec9e368d776441781bab2
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-18 14:49:22 -0600

    WIP

--- lib/world_cup.rb
+++ lib/world_cup.rb
@@ -9,10 +9,13 @@ class WorldCup
     active_players_at_position = []
     @teams.each do |team|
       if team.eliminated == false
-        team.players_by_position(position)
+        team.players.each do |player|
+          if team.player.position == position
+            active_players_at_position << player
+          end
+          return players_at_position
+        end
       end
-
-    end
-    return active_players_at_position
+    end 
   end
 end
