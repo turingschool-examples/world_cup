bfcbbe75392e586af26243a890a6595ec4abeb68
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-18 13:26:38 -0600

    add test methods & make tests pass

--- lib/team.rb
+++ lib/team.rb
@@ -1,8 +1,18 @@
 require "pry"
 
 class Team
-  attr_reader :country
+  attr_reader :country, :players, :eliminated
   def initialize(country)
     @country = country
+    @players = []
+    @eliminated = false
+  end
+
+  def eliminated?
+    @eliminated
+  end
+
+  def eliminated
+    @eliminated = true
   end
 end
--- test/team_test.rb
+++ test/team_test.rb
@@ -13,4 +13,16 @@ class TeamTest < Minitest::Test
     france = Team.new("France")
     assert_equal "France", france.country
   end
+
+  def test_it_has_default_zero_players
+    france = Team.new("France")
+    assert_equal [], france.players
+  end
+
+  def test_if_it_is_eliminated
+    france = Team.new("France")
+    assert_equal false, france.eliminated?
+    france.eliminated 
+    assert_equal true, france.eliminated?
+  end
 end
