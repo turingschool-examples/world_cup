1642f8d93a1e880d9f9f6035436783a06d624b44
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-07-18 14:52:33 -0600

    cleans up another test in world_cup_test

--- test/world_cup_test.rb
+++ test/world_cup_test.rb
@@ -30,21 +30,7 @@ class WorldCupTest < Minitest::Test
 
   def test_it_can_return_active_teams
     france = Team.new("France")
-    mbappe = Player.new("Kylian Mbappe", :forward)
-    griezmann = Player.new("Antoine Griezmann", :forward)
-    pogba = Player.new("Paul Pogba", :midfielder)
-    france.add_player(mbappe)
-    france.add_player(griezmann)
-    france.add_player(pogba)
-
     croatia = Team.new("Croatia")
-    modric = Player.new("Luka Modric", :midfielder)
-    perisic = Player.new("Ivan Perisic", :forward)
-    vida = Player.new("Domagoj Vida", :defender)
-    croatia.add_player(modric)
-    croatia.add_player(perisic)
-    croatia.add_player(vida)
-
     world_cup = WorldCup.new(2018, [france, croatia])
     expected = [france, croatia]
     actual = world_cup.teams_not_eliminated
