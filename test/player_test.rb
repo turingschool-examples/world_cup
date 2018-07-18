327b22f6d9df91d7a0b1ecadb08ec09f786ce1f5
Tim Fielder <fiel1139@gmail.com>
2018-07-18 13:10:01 -0600

    test for it exists in player.

--- test/player_test.rb
+++ test/player_test.rb
@@ -4,7 +4,12 @@ require "./lib/player.rb"
 require "pry"
 
 class PlayerTest < Minitest::Test
-  def initialize
+  def test_it_exists
+    pogba = Player.new("Paul Pogba", :midfielder)
+    assert_instance_of Player, pogba
+  end
+
+  def test_it_has_attributes
 
   end
 end
