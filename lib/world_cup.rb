11bf6d026719af790f6243c97b8a97150b466954
Tim Fielder <fiel1139@gmail.com>
2018-07-18 14:02:02 -0600

    adds active players by position test and class.

--- lib/world_cup.rb
+++ lib/world_cup.rb
@@ -1,5 +1,19 @@
 class WorldCup
-  def initialize
-    
+  attr_reader :year, :teams
+
+  def initialize(year, teams)
+    @year = year
+    @teams = teams
+  end
+
+  def active_players_by_position(position)
+    players_by_position = []
+    if @teams[0].eliminated? == false
+      players_by_position << @teams[0].players_by_position(position)
+    end
+    if @teams[1].eliminated? == false
+      players_by_position << @teams[1].players_by_position(position)
+    end
+    players_by_position.flatten(1)
   end
 end
