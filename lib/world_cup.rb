5f0a0c13d354368386a4c67214edd836b114ad08
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-18 14:45:49 -0600

    WIP

--- lib/world_cup.rb
+++ lib/world_cup.rb
@@ -5,4 +5,14 @@ class WorldCup
     @teams = teams
   end
 
+  def active_players_by_position(position)
+    active_players_at_position = []
+    @teams.each do |team|
+      if team.eliminated == false
+        team.players_by_position(position)
+      end
+
+    end
+    return active_players_at_position
+  end
 end
--- test/world_cup_test.rb
+++ test/world_cup_test.rb
@@ -54,4 +54,27 @@ class WorldCupTest < Minitest::Test
     assert_equal [france, croatia], world_cup.teams
   end
 
+  def test_can_list_active_players_by_position
+    france = Team.new("France")
+    mbappe = Player.new("Kylian Mbappe", :forward)
+    griezmann = Player.new("Antoine Griezmann", :forward)
+    pogba = Player.new("Paul Pogba", :midfielder)
+
+    france.add_player(mbappe)
+    france.add_player(griezmann)
+    france.add_player(pogba)
+
+    croatia = Team.new("Croatia")
+    modric = Player.new("Luka Modric", :midfielder)
+    perisic = Player.new("Ivan Perisic", :forward)
+    vida = Player.new("Domagoj Vida", :defender)
+
+    croatia.add_player(modric)
+    croatia.add_player(perisic)
+    croatia.add_player(vida)
+
+    world_cup = WorldCup.new(2018, [france, croatia])
+
+    assert_equal [mbappe, griezmann, perisic], world_cup.active_players_by_position(:forward)
+  end
 end
