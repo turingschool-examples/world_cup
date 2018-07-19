740bcbba151f62b3ba59203083313e8d7f0142c0
KellyMarcilliat <kelly.marcilliat@gmail.com>
2018-07-19 11:18:30 -0600

    Initial Commit - includes Iteration 1

---
+++ lib/player.rb
@@ -0,0 +1,11 @@
+class Player 
+  
+  attr_reader     :name,
+                  :position 
+                  
+  def initialize(name, position)
+    @name = name 
+    @position = position 
+  end 
+  
+end 
