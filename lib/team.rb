b8a959c4785fc9ff859de7bd080aff9faff890fe
Chi Tran <chidtran@gmail.com>
2019-02-05 18:44:24 -0700

    Update Team attr_accessor

--- lib/team.rb
+++ lib/team.rb
@@ -1,6 +1,6 @@
 class Team
-  attr_reader :country, :eliminated, :players
-  attr_writer :eliminated
+  attr_reader :country, :players
+  attr_accessor :eliminated
 
   def initialize(country)
     @country    = country 
