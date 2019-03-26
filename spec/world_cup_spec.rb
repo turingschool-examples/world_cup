5729b36a8794a591f988407b18f443a4bceaf1bb
davisc0801 <davisc1014@gmail.com>
2019-03-26 12:49:46 -0600

    Add test setup and world cup class

---
+++ spec/world_cup_spec.rb
@@ -0,0 +1,25 @@
+require "./lib/player"
+require "./lib/team"
+require "./lib/world_cup"
+
+describe WorldCup do
+
+  before do
+    @france = Team.new("France")
+    @mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+    @pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+    @france.add_player(@mbappe)
+    @france.add_player(@pogba)
+
+    @croatia = Team.new("Croatioa")
+    @modric = Player.new({name: "Luka Modric", position: "midfielder"})
+    @vida = Player.new({name: "Domagoj Vida", position: "defender"})
+    @croatia.add_player(@modric)
+    @croatia.add_player(@vida)
+
+    @world_cup = WorldCup.new(2018, [@france, @croatia])
+  end
+
+  
+
+end
