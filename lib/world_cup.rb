e0fe187520917798d206a2c080831d16dc56ed81
Alexandra Chakeres <alexchakeres@gmail.com>
2019-03-26 20:54:57 -0600

    Add init and test setup for WorldCup

--- lib/world_cup.rb
+++ lib/world_cup.rb
@@ -1,2 +1,10 @@
+require './lib/team'
+
 class WorldCup
+  attr_reader :year, :teams
+
+  def initialize(year, teams)
+    @year = year
+    @teams = teams
+  end
 end
--- test/world_cup_test.rb
+++ test/world_cup_test.rb
@@ -3,4 +3,23 @@ require 'minitest/pride'
 require './lib/world_cup'
 
 class WorldCupTest < Minitest::Test
+  def setup
+    @france = Team.new("France")
+    @mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+    @pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+    @croatia = Team.new("Croatia")
+    @modric = Player.new({name: "Luka Modric", position: "midfielder"})
+    @vida = Player.new({name: "Domagoj Vida", position: "defender"})
+
+    @france.add_player(@mbappe)
+    @france.add_player(@pogba)
+    @croatia.add_player(@modric)
+    @croatia.add_player(@vida)
+
+    @world_cup = WorldCup.new(2018, [@france, @croatia])
+  end
+
+  def test_it_exists
+    assert_instance_of WorldCup, @world_cup
+  end
 end
