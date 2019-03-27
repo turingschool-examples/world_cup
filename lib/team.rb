15241c3ac862ece017d0593090ddd195ad8d887b
Alexandra Chakeres <alexchakeres@gmail.com>
2019-03-26 20:34:29 -0600

    Add elimination to Team

--- lib/team.rb
+++ lib/team.rb
@@ -1,2 +1,15 @@
+require './lib/player'
+
 class Team
+  attr_reader :country
+  attr_accessor :eliminated
+
+  def initialize(country)
+    @country = country
+    @eliminated = false
+  end
+
+  def eliminated?
+    @eliminated
+  end
 end
--- test/team_test.rb
+++ test/team_test.rb
@@ -3,4 +3,29 @@ require 'minitest/pride'
 require './lib/team'
 
 class TeamTest < Minitest::Test
+  def test_it_exists
+    team = Team.new("France")
+
+    assert_instance_of Team, team
+  end
+
+  def test_it_inits_with_country
+    team = Team.new("France")
+
+    assert_equal "France", team.country
+  end
+
+  def test_it_inits_with_eliminated_false
+    team = Team.new("France")
+
+    assert_equal false, team.eliminated?
+  end
+
+  def test_it_can_be_eliminated
+    team = Team.new("France")
+
+    team.eliminated = true
+
+    assert_equal true, team.eliminated?
+  end
 end
