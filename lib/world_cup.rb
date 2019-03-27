daa00f0484ab10fbbb39fc02ff8ca4c4739d75c8
Alexandra Chakeres <alexchakeres@gmail.com>
2019-03-26 21:30:40 -0600

    Add all_players_by_position method to WorldCup

--- lib/world_cup.rb
+++ lib/world_cup.rb
@@ -8,15 +8,38 @@ class WorldCup
     @teams = teams
   end
 
-  def active_players_by_position(position)
+  def all_active_players
     active_teams = @teams.find_all {|team| !team.eliminated?}
-    all_active_players = []
+    all_active_players_ary = []
 
     active_teams.each do |active_team|
-      all_active_players << active_team.players
+      all_active_players_ary << active_team.players
     end
 
-    all_active_players.flatten!
-    all_active_players.find_all {|active_player| active_player.position == position}
+    all_active_players_ary.flatten!
+  end
+
+  def active_players_by_position(position)
+    all_active_players.find_all do |active_player|
+      active_player.position == position
+    end
+  end
+
+  def all_positions
+    all_pos_ary = all_active_players.map do |player|
+      player.position
+    end
+
+    all_pos_ary.uniq!
+  end
+
+  def all_players_by_position
+    new_hash = {}
+
+    all_positions.each do |position|
+      new_hash[position] = active_players_by_position(position)
+    end
+
+    new_hash
   end
 end
