7e1141f1b06ae62b50f2c914fcc3811357ade52c
Alexandra Chakeres <alexchakeres@gmail.com>
2019-03-26 20:25:31 -0600

    Initialize Player

--- test/player_test.rb
+++ test/player_test.rb
@@ -3,4 +3,16 @@ require 'minitest/pride'
 require './lib/player'
 
 class PlayerTest < Minitest::Test
+  def test_it_exists
+    player = Player.new({name: "Luka Modric", position: "midfielder"})
+
+    assert_instance_of Player, player
+  end
+
+  def test_it_inits_with_name_and_position
+    player = Player.new({name: "Luka Modric", position: "midfielder"})
+
+    assert_equal "Luka Modric", player.name   
+    assert_equal "midfielder", player.position
+  end
 end
