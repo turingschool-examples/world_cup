0b7ff11202baf70d26847158d34129bf55d19cb9
Alexandra Chakeres <alexchakeres@gmail.com>
2019-03-26 21:08:39 -0600

    Add active_players_by_position to WorldCup

--- test/world_cup_test.rb
+++ test/world_cup_test.rb
@@ -22,4 +22,19 @@ class WorldCupTest < Minitest::Test
   def test_it_exists
     assert_instance_of WorldCup, @world_cup
   end
+
+  def test_it_inits_with_year_and_teams
+    assert_equal 2018, @world_cup.year
+    assert_equal [@france, @croatia], @world_cup.teams
+  end
+
+  def test_active_players_by_position_returns_ary_of_non_eliminated_players_by_pos
+    assert_equal [@pogba, @modric], @world_cup.active_players_by_position("midfielder")
+  end
+
+  def test_active_players_by_position_returns_ary_of_non_eliminated_players_by_pos_after_elim
+    @croatia.eliminated = true
+
+    assert_equal [@pogba], @world_cup.active_players_by_position("midfielder")
+  end
 end
