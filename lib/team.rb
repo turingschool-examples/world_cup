3c1d4810d96cc7e5acb8e9b5443d66c4cd120ee7
Phil DeLong <philjdelong@gmail.com>
2019-10-14 23:08:03 -0600

    Start team_eliminated

--- lib/team.rb
+++ lib/team.rb
@@ -1,6 +1,21 @@
 class Team
+  attr_reader :eliminated
 
-  def initialize(team)
+  def initialize(team, eliminated = false)
+    @team = team
+    @eliminated = eliminated
+  end
+
+  def country
+    @team
+  end
+
+  def eliminated?
+    @eliminated
+  end
+
+  def eliminated
+    @team.eliminated = true
   end
 
 end
