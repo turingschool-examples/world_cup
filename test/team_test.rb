b261ab5ea543b549b4327baaab904e397aab54e7
Phil DeLong <philjdelong@gmail.com>
2019-10-14 23:37:22 -0600

    Finish iteration 3

--- test/team_test.rb
+++ test/team_test.rb
@@ -7,6 +7,8 @@ require'pry'
 class TeamTest < Minitest::Test
   def setup
     @team = Team.new('France')
+    @mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+    @pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
   end
 
   def test_it_exists
@@ -22,7 +24,27 @@ class TeamTest < Minitest::Test
   def test_it_is_elimniated
     # skip
     assert_equal false, @team.eliminated?
-    @team.eliminated? = true
+    @team.eliminated
     assert_equal true, @team.eliminated?
+  end
+
+  def test_it_has_players
+    # skip
+    assert_equal [], @team.players
+  end
+
+  def test_it_can_add_players
+    # skip
+    @team.add_player(@mbappe)
+    @team.add_player(@pogba)
+    assert_equal [@mbappe, @pogba], @team.players
+  end
+
+  def test_it_can_sort_by_position
+    # skip
+    @team.add_player(@mbappe)
+    @team.add_player(@pogba)
+    assert_equal @pogba, @team.players_by_position("midfielder")
+    assert_equal [], @team.players_by_position("midfielder")
   end
 end
