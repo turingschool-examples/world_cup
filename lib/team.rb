e7ffe8d2c9b85ade24d5d971ad315060880766ff
Phil DeLong <philjdelong@gmail.com>
2019-10-15 00:03:30 -0600

    Finish iteration 2

--- lib/team.rb
+++ lib/team.rb
@@ -1,9 +1,10 @@
 class Team
-  attr_reader :eliminated
+  attr_reader :eliminated, :players
 
-  def initialize(team, eliminated = false)
+  def initialize(team, eliminated = false, players = [])
     @team = team
     @eliminated = eliminated
+    @players = players
   end
 
   def country
@@ -15,7 +16,21 @@ class Team
   end
 
   def eliminated
-    @team.eliminated = true
+    @eliminated = true
+  end
+
+  def add_player(player)
+    @players << player
+  end
+
+  def players_by_position(position)
+    player_set = []
+    @players.each do |player|
+      if player.position == position
+        player_set << player
+      end
+    end
+    player_set
   end
 
 end
