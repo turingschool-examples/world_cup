9698a702ad561573088f89abc6b3899852b05226
Phil DeLong <philjdelong@gmail.com>
2019-10-14 22:00:41 -0600

    Write country, eliminated? tests

--- test/team_test.rb
+++ test/team_test.rb
@@ -11,8 +11,20 @@ class TeamTest < Minitest::Test
   end
 
   def test_it_exists
-    skip
+    # skip
     assert_instance_of Team, @team
+  end
+
+  def test_it_has_a_country
+    skip
+    assert_equal "France", @team.country
+  end
+
+  def test_it_is_elimniated
+    skip
+    assert_equal false, @team.eliminated?
+    team.eliminated = true
+    assert_equal true, @team.eliminated?
   end
 
 end
