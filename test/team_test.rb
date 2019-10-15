0b7efcaac0fcad9faf10fb3bd8e754839a909129
Phil DeLong <philjdelong@gmail.com>
2019-10-14 23:07:53 -0600

    Start team_eliminated

--- test/team_test.rb
+++ test/team_test.rb
@@ -5,9 +5,8 @@ require'./lib/team'
 require'pry'
 
 class TeamTest < Minitest::Test
-
   def setup
-    @team = Team.new("France")
+    @team = Team.new('France')
   end
 
   def test_it_exists
@@ -16,15 +15,14 @@ class TeamTest < Minitest::Test
   end
 
   def test_it_has_a_country
-    skip
-    assert_equal "France", @team.country
+    # skip
+    assert_equal 'France', @team.country
   end
 
   def test_it_is_elimniated
-    skip
+    # skip
     assert_equal false, @team.eliminated?
-    team.eliminated = true
+    @team.eliminated? = true
     assert_equal true, @team.eliminated?
   end
-
 end
