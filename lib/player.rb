5398ef64fcce3201bb78075012f28e40e565e06b
Alex Latham <alexlthm@gmail.com>
2020-02-10 21:43:29 -0700

    Add defi to player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,8 @@
 class Player
+  attr_reader :name, :position
 
-
+  def initialize(player_data_parameter)
+    @name = player_data_parameter[:name]
+    @position = player_data_parameter[:position]
+  end
 end
