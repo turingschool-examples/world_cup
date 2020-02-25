9f5ed83d9f29ea6710840cf60e92a1a7722f3647
Ashkan Abbasi <airashkan@yahoo.com>
2020-02-24 21:20:37 -0700

    Finish Iteration 1 and start 2

---
+++ lib/team.rb
@@ -0,0 +1,23 @@
+class Team
+  attr_reader :country
+  attr_accessor :eliminated, :players
+
+  def initialize(country)
+    @country = country
+    @eliminated = false
+    @players = []
+  end
+
+  def eliminated?
+    @eliminated
+  end
+
+  def add_player(player)
+    @players << player
+  end
+
+  def players_by_position(position)
+
+  end
+
+end
